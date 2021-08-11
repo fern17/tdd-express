98ec47556327c2c30f39e8c4504f484bc58b7743
Fernando Nellmeldin <fernando.nellmeldin@gmail.com>
2021-08-11 09:46:59 -0400

    hash password and test it

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-const app = require('./app');
+const app = require('./src/app');
+const sequelize = require('./src/config/database');
+
+sequelize.sync(); // create data base
 
 app.listen(3000, () => console.log('app is running!'));
--- src/app.js
+++ src/app.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const User = require('./user/User');
+const bcrypt = require('bcrypt');
 
 const app = express();
-
+app.use(express.json()); // parse body
 app.post('/api/1.0/users', (req, res) => {
-  return res.send({ message: 'User created' });
+  bcrypt.hash(req.body.password, 10).then((hash) => {
+    const user = {
+      username: req.body.username,
+      email: req.body.email,
+      password: hash,
+    };
+    User.create(user).then(() => {
+      return res.send({ message: 'User created' });
+    });
+  });
 });
 
 module.exports = app;
