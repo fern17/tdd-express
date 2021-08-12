57b5bac16440c04aa8d07d549953ae29bcbe1e3e
Fernando Nellmeldin <fernando.nellmeldin@gmail.com>
2021-08-12 08:28:53 -0400

    Password validation

--- __tests__/UserRegister.spec.js
+++ __tests__/UserRegister.spec.js
@@ -82,7 +82,6 @@ describe('User Registration', () => {
     expect(Object.keys(body.validationErrors)).toEqual(['username', 'email']);
   });
 
-  
   //it.each([
   //  ['username', 'Username cannot be null'],
   //  ['email', 'E-mail cannot be null'],
@@ -101,18 +100,28 @@ describe('User Registration', () => {
   //});
 
   it.each`
-    field         | expectedMessage
-    ${'username'} | ${'Username cannot be null'}
-    ${'email'}    | ${'E-mail cannot be null'}
-    ${'password'} | ${'Password cannot be null'}
-  `('returns $expectedMessage when $field is null', async ({ field, expectedMessage }) => {
+    field         | value              | expectedMessage
+    ${'username'} | ${null}            | ${'Username cannot be null'}
+    ${'username'} | ${'usr'}           | ${'Must have min 4 and max 32 characters'}
+    ${'username'} | ${'a'.repeat(33)}  | ${'Must have min 4 and max 32 characters'}
+    ${'email'}    | ${null}            | ${'E-mail cannot be null'}
+    ${'email'}    | ${'mail.com'}      | ${'E-mail is not valid'}
+    ${'email'}    | ${'user.mail.com'} | ${'E-mail is not valid'}
+    ${'email'}    | ${'user@mail'}     | ${'E-mail is not valid'}
+    ${'password'} | ${null}            | ${'Password cannot be null'}
+    ${'password'} | ${'P4ssw'}         | ${'Password must be at least 6 characters'}
+    ${'password'} | ${'alllowercase'}  | ${'Password must have at least 1 uppercase, 1 lowercase and 1 number'}
+    ${'password'} | ${'ALLUPPERCASE'}  | ${'Password must have at least 1 uppercase, 1 lowercase and 1 number'}
+    ${'password'} | ${'123456789'}     | ${'Password must have at least 1 uppercase, 1 lowercase and 1 number'}
+    ${'password'} | ${'lower44444'}    | ${'Password must have at least 1 uppercase, 1 lowercase and 1 number'}
+    ${'password'} | ${'UPPER444'}      | ${'Password must have at least 1 uppercase, 1 lowercase and 1 number'}
+  `('returns $expectedMessage when $field is $value', async ({ field, value, expectedMessage }) => {
     const user = {
       username: 'user1',
       email: 'user1@mail.com',
       password: 'P4ssword',
     };
-    user[field] = null;
-
+    user[field] = value;
     const response = await postUser(user);
     const body = response.body;
     expect(body.validationErrors[field]).toBe(expectedMessage);
