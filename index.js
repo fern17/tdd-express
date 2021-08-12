9111a99d55e6c9044e2bceba0e7eff2d1badacb1
Fernando Nellmeldin <fernando.nellmeldin@gmail.com>
2021-08-12 08:55:14 -0400

    validate unique emails with a custom validator

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require('./src/app');
 const sequelize = require('./src/config/database');
 
-sequelize.sync(); // create data base
+sequelize.sync({ force: true }); // create data base
 
 app.listen(3000, () => console.log('app is running!'));
